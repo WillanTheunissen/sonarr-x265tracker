60114e211384cbebadb77fdf32fe08bee47cf732
WillanTheunissen <38731414+WillanTheunissen@users.noreply.github.com>
2019-10-13 19:30:31 +0200

    V0.2
    config.js included

---
+++ config.js
@@ -0,0 +1,14 @@
+/*
+*   Menu URLS
+*/
+    var Plex = "http://mediaserver:32400/web/index.html"
+    var Tautalli = "http://mediaserver:8181/home"
+    var Sonarr = "http://mediaserver:8989/"
+    var Radarr = "http://mediaserver:7878/"
+    var QBittorrent = "mediaserver:20831"
+    var Jacektt = "http://mediaserver:9117/"
+/*
+*   API Credentials
+*/
+var SonarrAPI_url = "http://mediaserver:8989/api/"
+var SonarrAPI_key = "b2a4a458714447818c22494ca088471a"
